f923641c16f7014328996dbd20adefd230ba00e8
Harini Ashok <aharini2008@gmail.com>
2019-03-03 10:07:36 +0530

    Graph Final

--- Front-End/Display/hello.py
+++ Front-End/Display/hello.py
@@ -1,35 +1,58 @@
-import csv
+import pandas as pd
 import plotly as py
 import plotly.graph_objs as go
+from plotly.graph_objs import Scatter, Layout, Data, Figure
+
 from datetime import datetime
 
-moviefile = open("movies.csv","r")
-mv = csv.reader(moviefile)
-next(mv,None)
+data = pd.read_csv("ldr.csv")
+        
 
-userfile = open("users.csv","r")
-us=csv.reader(userfile)
-next(us,None)
 
-countmovies= len(list(mv))
-countusers= len(list(us))
-moviefile.seek(0)
-userfile.seek(0)
+countmovies= len(list(data))
+#data.seek(0)
 
 def Cloning(li1): 
     li_copy = list(li1) 
     return li_copy
 
-moviefile.seek(0)
-gencount = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-genre = ['Biography','Horror','Crime','Animation','Musical','Romance','Family','Comedy','Thriller','Mystery','History','Sci-Fi','Fantasy','Drama','Sport','War']
-listd = []
-for row in mv:    
-    listd = (row[8])
-    for j in range(0,16):
-        if listd.find(genre[j])!=-1:
-            gencount[j]+=1
+#data.seek(0)
 
-trace = go.Pie(labels=genre, values=gencount)
-py.offline.plot([trace], filename='genre_splitup.html',auto_open=False)
+
+random_x = data['Day']
+random_y = data['Percent']
+layoutt = go.Layout(
+    title='Percentage of Light on LDR in a given day',
+    xaxis=dict(
+        title='Days',
+        titlefont=dict(
+            family='Courier New, monospace',
+            size=18,
+            color='#7f7f7f'
+        )
+    ),
+    yaxis=dict(
+        title='Percent of Light of LDR',
+        titlefont=dict(
+            family='Courier New, monospace',
+            size=18,
+            color='#7f7f7f'
+        )
+    )
+)
+
+
+
+# Create a trace
+trace = go.Scatter(
+    x = random_x,
+    y = random_y,
+    name ='GG'
+    
+    
+    )
+data=[trace]
+fig = go.Figure(data, layout=layoutt)
+
+py.offline.plot(fig, filename='genre_splitup.html',auto_open=False)
 print("Generated Genre Splitup")
